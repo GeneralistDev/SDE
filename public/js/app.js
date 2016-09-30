e5e8aa53a1ec31372bb4be2c021288d791b3a282
Daniel Parker <dparker.tech@gmail.com>
2016-09-30 12:28:38 +1000

    Use moment to calculate days

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['angularMoment']);
 
 app.service("Entries", function ($http) {
 
@@ -23,10 +23,9 @@ app.service("Entries", function ($http) {
 });
 
 app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, $log, Entries) {
-
+  $scope.today = moment.utc();
 	$scope.fullName = null;
 	$scope.dob = null;
-	$scope.daysAlive = null;
 	$scope.entries = [];
 
 	$scope.init = function () {
@@ -35,13 +34,8 @@ app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, 
 
 	$scope.clickNewEntry = function () {
 		$log.info("Click New Entry");
-		$scope.daysAlive = $scope.daysSinceDate($scope.dob);
 		$scope.newEntry();
 	};
-
-	$scope.daysSinceDate = function (aDate) {
-		return 42;
-	}
 
 	// GET all entries
 	$scope.getEntries = function () {
@@ -60,9 +54,14 @@ app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, 
 
 	// POST a new entry
 	$scope.newEntry = function () {
+    var utcDOB = moment.utc([
+      $scope.dob.getFullYear(),
+      $scope.dob.getMonth(),
+      $scope.dob.getDate()
+      ]);
 		var entry = {
 			fullName: $scope.fullName,
-			dob: $scope.dob
+			dob: utcDOB
 		};
 
 		Entries.createEntry(entry).then(function (data) {
