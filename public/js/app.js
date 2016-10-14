171a0eb80cd63eee7b365c07831e949bc21603ee
Whillance, Samuel <Samuel.Whillance@contractor.ca.com>
2016-10-14 11:21:47 +1100

    Cleaned up ui code

--- public/js/app.js
+++ public/js/app.js
@@ -1,76 +1,48 @@
 var app = angular.module("myApp", ['angularMoment']);
 
+// Service
 app.service("Entries", function ($http) {
-
 	this.getEntries = function () {
-		return $http.get("/entries").
-		then(function (response) {
-			return response;
-		}, function (response) {
-			alert("Error finding entries.");
-		});
+		return $http.get("/entries");
 	}
-
 	this.createEntry = function (entry) {
-		return $http.post("/entries", entry).
-		then(function (response) {
-			return response;
-		}, function (response) {
-			alert("Error creating entry.");
-		});
+		return $http.post("/entries", entry);
 	}
-
 });
 
+// Controller
 app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, $log, Entries) {
-  $scope.today = moment.utc();
+	$scope.today = moment.utc();
 	$scope.fullName = null;
 	$scope.dob = null;
 	$scope.entries = [];
 
-	$scope.init = function () {
-		$scope.getEntries();
-	}
-
-	$scope.clickNewEntry = function () {
-		$log.info("Click New Entry");
-		$scope.newEntry();
-	};
-
-	// GET all entries
 	$scope.getEntries = function () {
 		Entries.getEntries().then(function (response) {
-			$log.info(response);
-			if (response.data) {
-				$scope.entries = response.data;
-			} else {
-				$scope.entries = response;
-			}
-
+			$log.info("Entries", response);
+			$scope.entries = response.data;
 		}, function (error) {
-			alert("Error getting entries");
+			alert("Error getting entries.");
 		});
 	};
 
-	// POST a new entry
-	$scope.newEntry = function () {
-    var utcDOB = moment.utc([
-      $scope.dob.getFullYear(),
-      $scope.dob.getMonth(),
-      $scope.dob.getDate()
-      ]);
+	$scope.createEntry = function () {
+		var utcDOB = moment.utc([
+			$scope.dob.getFullYear(),
+			$scope.dob.getMonth(),
+			$scope.dob.getDate()
+			]);
 		var entry = {
 			fullName: $scope.fullName,
 			dob: utcDOB
 		};
 
-		Entries.createEntry(entry).then(function (data) {
-			alert("Added entry");
+		Entries.createEntry(entry).then(function (response) {
+			alert("Entry created");
 			$scope.getEntries();
 		}, function (error) {
-			alert("Error posting entry");
+			alert("Error creating entry.");
 		});
 	};
-
 
 }]);
